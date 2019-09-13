bdddcf0d1134845ba9bcc0dcce96e02e6c80fcc8
Carl Liu <ao.liu@sypht.com>
2019-09-13 14:37:23 +1000

    restructure package

--- example/main.js
+++ example/main.js
@@ -1,17 +1,12 @@
-var Sypht = require("../lib/client");
+var sypht = require("../lib/client");
 const fs = require('fs');
-const utils = require('../lib/utils');
 
 const GENERIC = 'sypht.generic', DOCUMENT =  'sypht.document', INVOICE =  'sypht.invoice',BILL = 'sypht.bill', BANK = 'sypht.bank';
 
 async function main () {
-    var arr = utils.processAPIKey(process.env.SYPHT_API_KEY);
-    var sypht = new Sypht({
-        clientId : arr[0],
-        clientSecret : arr[1],
-    });
     var data = await sypht.fileUpload([INVOICE, DOCUMENT], fs.createReadStream('./sample_invoice.pdf'), 'sample_invoice.pdf');
     data = await sypht.fetchResults(data['fileId']);
+    var data = await sypht.fetchResults('d03d0c19-2cb9-4791-a520-afbcd7003967');
     console.log(JSON.stringify(data, null, 2));
 } 
 
