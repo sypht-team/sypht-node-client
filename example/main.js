f7f1b10fc2d99b131cc668b8a65cab1c0eb6d2d2
Jon De Santos <jon@sypht.com>
2020-07-01 12:53:24 +1000

    update example file

--- example/main.js
+++ example/main.js
@@ -1,11 +1,16 @@
-var sypht = require('sypht-node-client');
+var sypht = require('../../sypht-node-client');
 
 const GENERIC = 'sypht.generic', DOCUMENT =  'sypht.document', INVOICE =  'sypht.invoice',BILL = 'sypht.bill', BANK = 'sypht.bank';
 
 async function main () {
     var data = await sypht.fileUpload(['sypht.invoice', 'sypht.document'], './sample_invoice.pdf');
     data = await sypht.fetchResults(data['fileId']);
-    console.log(JSON.stringify(data, null, 2));
+    return JSON.stringify(data, null, 2);
 } 
 
-main();
+main().then(
+    data => console.log(data)
+)
+.catch(
+    e => console.log(e)
+);
